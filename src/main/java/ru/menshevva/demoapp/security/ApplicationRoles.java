71c858bcdbfd16a400cd92e2cd8baea57d22a024
menshevva <mvawork@mail.ru>
2025-11-16 17:37:09 +0300

    Удалены не используемые роли

--- src/main/java/ru/menshevva/demoapp/security/ApplicationRoles.java
+++ src/main/java/ru/menshevva/demoapp/security/ApplicationRoles.java
@@ -3,6 +3,6 @@ package ru.menshevva.demoapp.security;
 public class ApplicationRoles {
 
     public static final String ROLE_ADMIN = "ADMIN";
-    public static final String ROLE_USER = "USER";
-    public static final String ROLE_TEAM_LEADER = "TEAM_LEADER";
+    public static final String ROLE_CLIENTS = "CLIENTS";
+
 }
